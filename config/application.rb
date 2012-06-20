0af85b2641fd75dc08fb52ed6a450104ab581b2a
Chris Searle <chris@chrissearle.org>
2012-06-20 22:28:14 +0200

    Add dalli config for static assets

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,9 @@ module Gear
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    config.assets.initialize_on_precompile = false
+
+    config.cache_store = :dalli_store
   end
 end
